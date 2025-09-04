47e59481d7a93f75165f96e7336717320fc81668
Cleiton Trentin <cleiton.trentin@gmail.com>
2025-09-04 18:55:31 -0300

    Camada de Servico, registro de componentes

---
+++ src/main/java/com/educandoweb/corse/service/UserServive.java
@@ -0,0 +1,28 @@
+package com.educandoweb.corse.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.corse.entities.User;
+import com.educandoweb.corse.repositories.UserRepository;
+
+@Service
+public class UserServive {
+
+	@Autowired
+	private UserRepository repository;
+	
+	//operação para buscar todos os usuários
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	//operação para buscar usuário pelo id
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
