4b50717163db34db9784042efa348d3717b60ea2
Cleiton Trentin <cleiton.trentin@gmail.com>
2025-09-05 12:46:46 -0300

    Enum OrderStatus

---
+++ src/main/java/com/educandoweb/corse/entities/enums/OrderStatus.java
@@ -0,0 +1,32 @@
+package com.educandoweb.corse.entities.enums;
+
+public enum OrderStatus {
+	
+	WHAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPEDD(3),
+	DELIVERED(4),
+	CANCELAD(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		
+		for (OrderStatus value : OrderStatus.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalidad OrderStatus code");
+		
+	}
+
+}
